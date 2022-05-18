b60fb7fd4eda99ff8ad091703a54bcfeb2d35de2
huycan <vohuy21032001@gmail.com>
2022-05-18 12:10:42 +0700

    add locators

---
+++ POM_project/Locators/locators.py
@@ -0,0 +1,10 @@
+class Locators():
+
+    # ID_login_page
+    username_textbox_id = "txtUsername"
+    password_textbox_id = "txtPassword"
+    login_button_id = "btnLogin"
+
+    #logout
+    welcome_link_id = "welcome"
+    logout_link_linkText = "Logout"
